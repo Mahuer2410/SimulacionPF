67b214431584feadf789dee7b59e100df88284a9
Mahuer2410 <ezequielrodriguez2410@gmail.com>
2024-07-09 23:18:12 -0400

    moviemientos del dependiente a las instancias de los servidores

--- Assets/Scripts/DependienteController.cs
+++ Assets/Scripts/DependienteController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class DependienteController : MonoBehaviour
 {
     private List<Transform> targetPoints = new List<Transform>(); // Lista de puntos de destino (mesas)
-    private Transform originalPosition; // Posición original del NPC
+    private Vector3 originalPosition; // Posición original del NPC
     private bool movingToTarget = true; // Indica si se mueve hacia el destino o la posición original
+    private bool visitedAllTargets = false; //Comprueba si se han visitado todos los puntos de destino
 
     public float movementSpeed = 2f; // Velocidad de movimiento
     private float waitTime; // Tiempo de espera aleatorio
@@ -16,27 +17,22 @@ public class DependienteController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        originalPosition = transform;
-
+        originalPosition = transform.position;
+        
         // Establecemos un tiempo de espera aleatorio entre 1 y 5 segundos
         waitTime = Random.Range(1f, 5f);
         currentTime = waitTime;
-        
-        // Buscamos todas las instancias del objeto "MesaTecnica"
+        // Buscamos todas las instancias del objeto "MesaTecnica(Clone)"
         GameObject[] mesaTecnicaInstances = GameObject.FindGameObjectsWithTag("MesaTecnica");
         foreach (var instance in mesaTecnicaInstances)
         {
             targetPoints.Add(instance.transform);
         }
-        targetPoints.Add(originalPosition);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
         // Esperamos antes de comenzar a mover al NPC
         if (currentTime > 0)
         {
@@ -44,18 +40,43 @@ public class DependienteController : MonoBehaviour
             return; // Salimos del Update sin hacer nada más
         }
 
-        // Movimiento hacia el punto de destino actual
-        Vector3 directionToTarget = (targetPoints[currentTargetIndex].position - transform.position).normalized;
-        transform.Translate(directionToTarget * movementSpeed * Time.deltaTime);
-
-        // Verificamos si hemos llegado al punto de destino
-        if (Vector3.Distance(transform.position, targetPoints[currentTargetIndex].position) <= 0.1f)
+        // Si ya ha visitado todos los puntos de destino, mueve al NPC hacia la posición original
+        if (visitedAllTargets)
         {
-            movingToTarget = !movingToTarget; // Cambiamos la dirección
-            currentTargetIndex = (currentTargetIndex + 1) % targetPoints.Count; // Cambiamos al siguiente punto de destino
-            // Reiniciamos el tiempo de espera aleatorio
-            waitTime = Random.Range(1f, 5f);
-            currentTime = waitTime;
+            Vector3 directionToOriginal = (originalPosition - transform.position).normalized;
+            transform.Translate(directionToOriginal * movementSpeed * Time.deltaTime);
+
+            // Verificamos si hemos llegado a la posición original
+            if (Vector3.Distance(transform.position, originalPosition) <= 0.1f)
+            {
+                // Reiniciamos el tiempo de espera aleatorio
+                waitTime = Random.Range(1f, 5f);
+                currentTime = waitTime;
+                visitedAllTargets = false; // Reiniciamos la variable
+            }
+        }
+        else
+        {
+            // Movimiento hacia el punto de destino actual
+            Vector3 directionToTarget = (targetPoints[currentTargetIndex].position - transform.position).normalized;
+            transform.Translate(directionToTarget * movementSpeed * Time.deltaTime);
+
+            // Verificamos si hemos llegado al punto de destino
+            if (Vector3.Distance(transform.position, targetPoints[currentTargetIndex].position) <= 0.1f)
+            {
+                movingToTarget = !movingToTarget; // Cambiamos la dirección
+                currentTargetIndex = (currentTargetIndex + 1) % targetPoints.Count; // Cambiamos al siguiente punto de destino
+                                                                                    // Reiniciamos el tiempo de espera aleatorio
+                waitTime = Random.Range(1f, 5f);
+                currentTime = waitTime;
+
+                // Si hemos visitado todos los puntos de destino, activamos la variable
+                if (currentTargetIndex == 0)
+                {
+                    visitedAllTargets = true;
+                }
+            }
         }
     }
+
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,7 +8,7 @@ public class Spawner : MonoBehaviour
     public int numberOfInstances; // Cantidad de instancias deseadas
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         for (int i = 0; i < numberOfInstances; i++)
         {
